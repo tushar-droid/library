db226cc349ba54a48f0ad7ba0dceeaab6f319a4a
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-07-06 03:06:20 -0700

    added some books

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ Book.prototype.info = function(){
     return (`${this.name} by ${this.author} with ${this.pages} is ${this.read_status}`);
 }
 
+let book1 = new Book('Gone with the Wind', 'Margret Mithcell', 290, 'read');
+let book2 = new Book('Jane Eyre', 'Charlotte Bronte', 243, 'not read');
+let book3 = new Book('pride and Prejudice', 'Jane Austen', 320, 'read');
+let book4 = new Book('To kill a Mockingbird', 'Harper Lee', 340, 'not read');
+let book5 = new Book('Little Women', 'Louisa May Alcott', 124, 'read');
+
+
+
+
 
 
 
