b800f64506059bdb4a19fd0d004e6071d52787af
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-07-07 02:02:07 -0700

    Cards Working Need to work on blank card

--- script.js
+++ script.js
@@ -11,25 +11,51 @@ Book.prototype.info = function(){
     return (`${this.name} by ${this.author} with ${this.pages} is ${this.read_status}`);
 }
 
-let book1 = new Book('Gone with the Wind', 'Margret Mithcell', 290, 'read');
-let book2 = new Book('Jane Eyre', 'Charlotte Bronte', 243, 'not read');
-let book3 = new Book('pride and Prejudice', 'Jane Austen', 320, 'read');
-let book4 = new Book('To kill a Mockingbird', 'Harper Lee', 340, 'not read');
-let book5 = new Book('Little Women', 'Louisa May Alcott', 124, 'read');
+let book1 = new Book('Gone with the Wind ', 'Margret Mithcell', 290, 'Read');
+let book2 = new Book('Jane Eyre', 'Charlotte Bronte', 243, 'Not Read');
+let book3 = new Book('pride and Prejudice', 'Jane Austen', 320, 'Read');
+let book4 = new Book('To kill a Mockingbird', 'Harper Lee', 340, 'Not Read');
+let book5 = new Book('Little Women', 'Louisa May Alcott', 124, 'Read');
+
 
 let allBooks = [book1, book2, book3, book4, book5];
 
 
-displayBooks(allBooks)
+allBooks.forEach(book => {
+    DisplayBooks(book);
+});
+EmptyCard();
 
-
-function displayBooks(allBooks){
-    allBooks.forEach(book => {
-
-    });
+function DisplayBooks(book){
+    const container = document.querySelector('.main-content');
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const book_name= document.createElement('h2');
+    const author_name = document.createElement('h4');
+    const pages = document.createElement('p');
+    const r_status = document.createElement('p');   
+    book_name.textContent = book.name;
+    author_name.textContent= `:- ${book.author}`;
+    pages.textContent = `Total pages: ${book.pages}`;
+    r_status.textContent = book.read_status;
+    card.appendChild(book_name);
+    card.appendChild(author_name);
+    card.appendChild(pages);
+    card.appendChild(r_status);
+    container.appendChild(card);
 
 }
 
 
+function EmptyCard(){
+    const container = document.querySelector('.main-content');
+    const card = document.createElement('div');
+    card.classList.add('card', 'empty');
+    const button = document.createElement('button')
+    button.type = 'button';
+    button.innerHTML = `<img src = 'Icons/add_icon.svg' alt='addicon' class='add-icon'/>`;
+    card.appendChild(button);
+    container.appendChild(card);
+}
 
 
