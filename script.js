020cbed554407069cdb432941979e08baa20d46a
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-07-08 01:23:38 -0700

    Form added and form buttons working now

--- script.js
+++ script.js
@@ -51,11 +51,85 @@ function DisplayBooks(book){
 function EmptyCard(){
     const container = document.querySelector('.main-content');
     const card = document.createElement('div');
-    card.classList.add('card');
+    card.classList.add('card', 'empty');
     const button = document.createElement('div')
-    button.innerHTML = `<img src = 'Icons/add_icon.svg' alt='addicon' class='add-icon'/>`;
+    button.innerHTML = `<img src = 'Icons/add_icon.svg' alt='addicon' class='add-icon' id='add-card'/>`;
     card.appendChild(button);
     container.appendChild(card);
+    
+
+const addmore = document.querySelector('#add-card');
+addmore.addEventListener('click', AddCard);
+
+}
+
+
+
+
+function AddCard(){
+    const container = document.querySelector('.empty');
+    container.textContent = '';
+    //creating the form
+    const newForm = document.createElement('Form');
+    newForm.setAttribute("method", "post");
+    newForm.setAttribute("action", "");
+
+    //getting all the required pieces
+    const bookname = document.createElement('input');
+    const authorname = document.createElement('input');
+    const totalpages = document.createElement('input');
+    const readstatus = document.createElement('input');
+    const submitbtn = document.createElement('input');
+    const cancelbtn = document.createElement('input');
+
+    //Setting the attributes for the Book Name
+    bookname.setAttribute('type', 'text');
+    bookname.setAttribute('name', 'bookname');
+    bookname.setAttribute('placeholder','Book Name' );
+
+    //Setting the attributes for the author name
+    authorname.setAttribute('type', 'text');
+    authorname.setAttribute('name', 'authorname');
+    authorname.setAttribute('placeholder','Author Name');
+
+    //setting the attributes for the pages
+    totalpages.setAttribute('type', 'text');
+    totalpages.setAttribute('name', 'pages');
+    totalpages.setAttribute('placeholder','total # of pages' );
+
+    //setting the attributs for the read status
+    readstatus.setAttribute('type', 'text');
+    readstatus.setAttribute('name', 'readstatus');
+    readstatus.setAttribute('placeholder','Read or Not Read' );
+
+    //Setting the attributes for the submit button
+    submitbtn.setAttribute('type', 'submit');
+    submitbtn.setAttribute('value', 'Submit');
+    submitbtn.setAttribute('id', 'submit-btn');    
+
+    //Setting the attributes for the cancel button
+    cancelbtn.setAttribute('type', 'button');
+    cancelbtn.setAttribute('value', 'cancel'); 
+    cancelbtn.setAttribute('id', 'cancel-btn');    
+
+    //This will work on sumit click
+    newForm.addEventListener('submit', function(event){
+        event.preventDefault();
+    });
+
+    cancelbtn.addEventListener('click', function(event) {
+        container.remove();
+        EmptyCard();
+    })
+
+    newForm.appendChild(bookname);
+    newForm.appendChild(authorname);
+    newForm.appendChild(totalpages);
+    newForm.appendChild(readstatus);
+    newForm.appendChild(submitbtn);
+    newForm.appendChild(cancelbtn);
+    container.appendChild(newForm);
+
 }
 
 
